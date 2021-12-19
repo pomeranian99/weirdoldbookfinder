03b747c3057e9cf2242c5221cbfeea01994ee3b6
Glitch (glitch-hello-node) <none>
2021-12-19 03:25:54 +0000

    🎢🌝 Checkpoint ./server.js:291626/214

--- server.js
+++ server.js
@@ -13,8 +13,9 @@ app.get("/", async function(request, response) {
 
 
 app.get("/bookme", async function(request, response) {
-  console.log("the bookme function has fired");
-  response.send("index.html");
+  console.log("the bookme URL has fired");
+  let results = await bookMe();
+  response.send(results.data.items[0]);
 });
 
 
@@ -25,8 +26,9 @@ const listener = app.listen(process.env.PORT, () => {
 
 
 async function bookMe() {
+  console.log("the bookMefunction has fired");
   try {
-    return await axios.get('https://www.googleapis.com/books/v1/volumes?q=machinery+silly&filter=free-ebooks');
+    return await axios.get('https://www.googleapis.com/books/v1/volumes?q=machinery');
   } catch (err) {
     console.error(err);
   }
