6f9e4f7c7eed3b472a899c5a4cb74f8eec3bad4c
Glitch (glitch-hello-node) <none>
2021-12-19 03:14:04 +0000

    😼🐧 Checkpoint ./server.js:291626/465

--- server.js
+++ server.js
@@ -12,6 +12,23 @@ app.get("/", async function(request, response) {
 });
 
 
+app.get("/bookme", async function(request, response) {
+  console.log("the bookme function has fired");
+  response.send("index.html");
+});
+
+
+
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
+
+
+async function bookMe() {
+  try {
+    return await axios.get('https://www.googleapis.com/books/v1/volumes?q=machinery+silly&filter=free-ebooks');
+  } catch (err) {
+    console.error(err);
+  }
+  // https://www.googleapis.com/books/v1/volumes?q=machinery+silly&filter=free-ebooks
+}
