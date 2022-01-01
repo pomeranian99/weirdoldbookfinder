ac699d758ef86b053986890c6037280db25d5b28
Glitch (glitch-hello-node) <none>
2022-01-01 02:59:32 +0000

    🐿🕵 Checkpoint ./server.js:291626/163

--- server.js
+++ server.js
@@ -14,14 +14,15 @@ app.get("/", async function(request, response) {
 app.get("/bookMe", async function(request, response) {
   let queryPhrase = request.query.words;
   let results = await bookMe(queryPhrase);
-  // console.log(results.data.items);
+  console.log(results.data.items);
   let pre1924Books = [];
-  for (let i = 0; i < length.results.data.items; i++) {
+  for (let i = 0; i < results.data.items.length; i++) {
     if (Number(results.data.items[i].volumeInfo.publishedDate) < 1924) {
       pre1924Books.push(results.data.items[i].id)
     }
   }
-  console.log(pre1924Books);
+  // console.log(pre1924Books);
+  let randoBook = pre1924Books[Math.floor(Math.random() * pre1924Books.length)];
   response.send(results.data.items[0].id);
 });
 
@@ -33,7 +34,7 @@ const listener = app.listen(process.env.PORT, () => {
 
 async function bookMe(x) {
   try {
-    return await axios.get('https://www.googleapis.com/books/v1/volumes?q=' + x);
+    return await axios.get('https://www.googleapis.com/books/v1/volumes?q=' + x + '&filter=free-ebooks');
   } catch (err) {
     console.error(err);
   }
