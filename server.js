1d6f968552cb4a933904f9f342bd423794f8eadb
Glitch (glitch-hello-node) <none>
2022-01-19 22:42:36 +0000

    🐃😋 Checkpoint ./views/index.pug:291626/15 ./server.js:291626/464

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const axios = require("axios");
 const express = require("express");
 const app = express();
+const bodyParser = require('body-parser')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
 app.set("view engine", "pug");
 
 app.use(express.static("public"));
@@ -15,10 +21,11 @@ app.get("/pugtext", async function(request, response) {
 });
 
 
-app.get("/bookMe", async function(request, response) {
+app.post("/bookMe", async function(request, response) {
   // probably should have put all this logic into a separate function instead of leaving 
   // it here in the routing but i'm lazy and hey it works lol
-  let queryPhrase = request.query.words;
+  let queryWeGot = request.query.searchText;
+  let queryPhrase = queryWeGot.replace(/ /g, "+")
   let results = await bookMe(queryPhrase);
   let pre1924Books = [];
   for (let i = 0; i < results.data.items.length; i++) {
@@ -28,7 +35,7 @@ app.get("/bookMe", async function(request, response) {
   }
   console.log(pre1924Books);
   let randoBook = pre1924Books[Math.floor(Math.random() * pre1924Books.length)];
-  response.send(randoBook);
+  response.render("index", { bookRetrieved: randoBook });
 });
 
 
